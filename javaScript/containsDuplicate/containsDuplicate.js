145f315a7f29ff77e92fbd2ad0dda2413c17cd27
Kresten24 <ronie-8@hotmail.com>
2022-08-22 07:46:49 -0400

    contains duplicate solution

---
+++ javaScript/containsDuplicate/containsDuplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    let obj={}
+    for(let i=0; i<nums.length; i++){
+        if(!obj[nums[i]]){
+            obj[nums[i]]=1
+        }else
+            return true
+    }
+    return false
+};
