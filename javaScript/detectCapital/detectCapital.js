9c194354b96b7110594cf1d97b0bb16d061571bb
Kresten24 <ronie-8@hotmail.com>
2022-08-24 10:09:37 -0400

    detect capital solution

---
+++ javaScript/detectCapital/detectCapital.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+ var detectCapitalUse = function(word) {
+    if(word.toUpperCase()===word)
+        return true
+    else if(word.toLowerCase()===word)
+        return true
+    else if(word.charAt(0).toUpperCase()+word.slice(1).toLowerCase()===word)
+        return true
+    else
+        return false
+};
